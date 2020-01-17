606565dda239f203f484368c87776d7b27cb8f53
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-17 20:04:13 +0300

    еще подумал

--- src/DropdownList/DropdownList.jsx
+++ src/DropdownList/DropdownList.jsx
@@ -6,23 +6,34 @@ class DropdownList extends React.Component {
     this.state = {
       data: this.props.data
     };
+    this.createListData = this.createListData.bind(this);
+    this.createButton = this.createButton.bind(this);
+    this.createHidden = this.createHidden.bind(this);
+  }
+
+  createButton = (item) => {
+    return <button className='accordion'>{item}</button>
+  }
+
+  createHidden = (item) => {
+    return <div className='panel'>{item}</div>
+  }
+
+  createListData = (data) => {
+    for (let [key, value] of Object.entries(data)) {
+      if (value && typeof(value) == "object") {
+        console.log(key, value);
+        this.createButton(key);
+        this.createListData(value);
+      } else {
+        this.createHidden(key);
+        console.log(key);
+      }
+    }
   }
 
   render() {
-    const createListData = (data) => {
-      Object.keys(data).map(item => {
-          if (data[item]) {
-            console.log(item, data[item]);
-            return <button className='accordion'>{item}</button>
-            createListData(data[item])
-          } else {
-            return <div className='panel'>{item}</div>
-          }
-        }
-      )
-    }
-
-    return <div>{createListData(this.state.data)}</div>
+    return <div>{this.createListData(this.state.data)}</div>
   }
 }
 
